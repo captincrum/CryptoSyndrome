f69cfacd2b1b79e570a51cd944fde8c80a9c992b
Shawn McCrum <xmccrum@gmail.com>
2017-11-01 17:56:13 -0400

    Update publicConnect.py

--- publicConnect.py
+++ publicConnect.py
@@ -32,23 +32,3 @@ def public_account(fld, ticker_id):
     bid_num   = bid[2]
     bid_array.append((bid_price))
   print(bid_array)
-#    print(bid_price, bid_size, bid_order)
-
-#  keys_values(ticker_bid)
-#  len_order    = len(ticker_bid)    # 50
-#  len_ticker   = len(ticker_bid[0]) # 3
-#  count_i      = 0
-#  count_ii     = 0
-#  bid_volume   = []
-#  ask_volume   = []
-#  while count_ii < len_ticker:
-#    while count_i < len_order:
-#      bid_volume.append(ticker_bid[count_i][0])
-#      bid_volume.append(ticker_bid[count_i][1])
-#      ask_volume.append(ticker_ask[count_i][0])
-#      ask_volume.append(ticker_ask[count_i][1])
-#      count_i += 1
-#    count_ii  += 1
-#    print(bid_volume, '\n', ask_volume)
-#
-#    count_i *= 0
